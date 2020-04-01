79bb26ec62b0dbdcae2bacb74a4e042583d6d676
Souhail CHATTI <souhail.chatti@antalis.com>
2020-04-01 04:29:27 +0200

    Version 2

---
+++ test_data.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+
+def read_test_data():
+	 #load data
+    return pd.read_csv("C:/Users/schatti/OneDrive - ANTALIS INTERNATIONAL/Bureau/Digit Recognizer/Data/test.csv")
