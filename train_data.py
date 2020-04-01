5b8cae35353d13eca1265c9720c7835d8e92f03b
Souhail CHATTI <souhail.chatti@antalis.com>
2020-04-01 04:28:17 +0200

    Version 1

---
+++ train_data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+
+
+def read_train_data():
+        #load data
+        return pd.read_csv("C:/Users/schatti/OneDrive - ANTALIS INTERNATIONAL/Bureau/Digit Recognizer/Data/train.csv")
+
+
